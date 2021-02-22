4f6ae9121f1b78f858c3d92071ae096e4563397b
Darshan-26 <79460770+Darshan-26@users.noreply.github.com>
2021-02-22 21:48:51 +0545

    Add files via upload

---
+++ 2.Area of Triangle.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<math.h>
+
+    float area(float b, float h)
+    {
+        float a;
+        a=(b*h)/2 ;
+        return a;
+    }
+int main()
+{
+
+    float b,h,ar;
+    printf("\nEnter the value of base and height \n");
+    scanf("%f %f", &b, &h);
+    ar= area(b,h);
+    printf("\nArea of the triangle is %f", ar);
+    return 0;
+}
